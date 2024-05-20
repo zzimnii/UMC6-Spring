2cce01874ee05b4c14b9895b857d30ff27a46417
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-05-20 16:44:41 +0900

    [FEAT] mission8

---
+++ src/main/java/umc/spring/service/TempService/TempQueryService.java
@@ -0,0 +1,9 @@
+package umc.spring.service.TempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+public interface TempQueryService {
+
+    void CheckFlag(Integer flag);
+}
