dbdd9cc9be408240cc2c945744f56d83f6e0f916
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-05-21 19:14:15 +0900

    [FIX] ApiResponse 변

--- src/main/java/umc/spring/service/TempService/TempQueryService.java
+++ src/main/java/umc/spring/service/TempService/TempQueryService.java
@@ -1,8 +1,5 @@
 package umc.spring.service.TempService;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 public interface TempQueryService {
 
     void CheckFlag(Integer flag);
